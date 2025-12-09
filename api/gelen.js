e92340b69ea0602bd20bcce984548261e47f6dd3
sinanzenbil-lgtm <sinanzenbil@gmail.com>
2025-12-10 02:12:57 +0300

    Refactor SOAP request to JSON format

--- api/gelen.js
+++ api/gelen.js
@@ -1,5 +1,4 @@
 const axios = require('axios');
-const xml2js = require('xml2js');
 
 module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Credentials', true);
@@ -21,60 +20,44 @@ module.exports = async (req, res) => {
         
         const apiUrl = 'https://efatura.uyumsoft.com.tr/Services/Integration';
 
-        const soapRequest = `<?xml version="1.0" encoding="utf-8"?>
-<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">
-    <s:Header>
-        <Security xmlns="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
-            <UsernameToken>
-                <Username>${username}</Username>
-                <Password>${password}</Password>
-            </UsernameToken>
-        </Security>
-    </s:Header>
-    <s:Body>
-        <GetInboxInvoiceList xmlns="http://tempuri.org/">
-            <invoiceListRequest>
-                <Taken xmlns="http://schemas.datacontract.org/2004/07/Uyumsoft.EFatura.Proxy.Integration">All</Taken>
-            </invoiceListRequest>
-        </GetInboxInvoiceList>
-    </s:Body>
-</s:Envelope>`;
+        const requestBody = {
+            "Action": "GetInboxInvoiceList",
+            "parameters": {
+                "userInfo": {
+                    "Username": username,
+                    "Password": password
+                },
+                "query": {
+                    "CreateStartDate": "2024-01-01T00:00:00.000",
+                    "CreateEndDate": "2025-12-31T23:59:59.999"
+                }
+            }
+        };
 
-        const response = await axios.post(apiUrl, soapRequest, {
+        const response = await axios.post(apiUrl, requestBody, {
             headers: {
-                'Content-Type': 'text/xml; charset=utf-8',
-                'SOAPAction': 'http://tempuri.org/IIntegration/GetInboxInvoiceList'
+                'Content-Type': 'application/json'
             },
             timeout: 60000
         });
 
-        const parser = new xml2js.Parser({ 
-            explicitArray: false, 
-            ignoreAttrs: false,
-            tagNameProcessors: [xml2js.processors.stripPrefix]
-        });
-
         let invoices = [];
-        let parsedResult = await parser.parseStringPromise(response.data);
         
-        const body = parsedResult?.Envelope?.Body;
-        const responseData = body?.GetInboxInvoiceListResponse?.GetInboxInvoiceListResult;
-        
-        if (responseData?.Value?.InvoiceInfo) {
-            const invoiceData = responseData.Value.InvoiceInfo;
+        if (response.data?.Value) {
+            const invoiceData = response.data.Value;
             invoices = Array.isArray(invoiceData) ? invoiceData : [invoiceData];
         }
 
         const formattedInvoices = invoices.map(inv => ({
             invoiceNumber: inv.InvoiceNumber || inv.ID || '-',
             uuid: inv.UUID || '-',
-            invoiceDate: inv.IssueDate || inv.InvoiceDate || '-',
-            senderName: inv.SenderName || '-',
-            senderVkn: inv.SenderVkn || '-',
-            amount: parseFloat(inv.PayableAmount) || 0,
-            taxAmount: parseFloat(inv.TaxTotal) || 0,
-            totalAmount: parseFloat(inv.PayableAmount) || 0,
-            status: inv.Status || '-'
+            invoiceDate: inv.IssueDate || inv.InvoiceDate || inv.CreateDate || '-',
+            senderName: inv.SenderName || inv.SenderTitle || '-',
+            senderVkn: inv.SenderVkn || inv.SenderIdentifier || '-',
+            amount: parseFloat(inv.PayableAmount) || parseFloat(inv.TotalAmount) || 0,
+            taxAmount: parseFloat(inv.TaxTotal) || parseFloat(inv.TaxAmount) || 0,
+            totalAmount: parseFloat(inv.PayableAmount) || parseFloat(inv.TotalAmount) || 0,
+            status: inv.Status || inv.InvoiceStatus || '-'
         }));
 
         return res.status(200).json({
